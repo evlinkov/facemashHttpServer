ecb9a98aa12c281e9afed4e650625b225b3d9056
Evgeniy Linkov <evlinkov@gmail.com>
2018-09-28 01:22:03 +0300

    tomcat, flyway

---
+++ src/main/java/peak/config/TomcatConfig.java
@@ -0,0 +1,18 @@
+package peak.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory =
+                new TomcatEmbeddedServletContainerFactory();
+        return factory;
+    }
+
+}
